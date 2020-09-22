97e683ec69d4f9d2a9f741c17ceb0c5ecaeeaf1e
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-09-22 00:17:03 -0600

    Se creo el archivo donde se crearan los test

---
+++ src/test/java/Proceso/CRISTIAN_MANUEL_CASTELLANOS_AJTUN_201709177.java
@@ -0,0 +1,29 @@
+package Proceso;
+
+import Objetos.Carrera;
+import Objetos.RegistroAcademico;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+class CRISTIAN_MANUEL_CASTELLANOS_AJTUN_201709177 {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
