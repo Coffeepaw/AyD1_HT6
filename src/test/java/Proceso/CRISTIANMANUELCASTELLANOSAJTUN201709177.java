3e25fcd381e5d4011ae36d01264947bc69e8e85e
Cristian Castellanos <cris.manu.caste7@gmail.com>
2020-09-30 18:23:15 -0600

    Se elimino codigo innecesario

--- src/test/java/Proceso/CRISTIANMANUELCASTELLANOSAJTUN201709177.java
+++ src/test/java/Proceso/CRISTIANMANUELCASTELLANOSAJTUN201709177.java
@@ -51,16 +51,7 @@ class CRISTIANMANUELCASTELLANOSAJTUN201709177 {
 		AsignarHorario asignarl = new AsignarHorario();
 		assertEquals(asignarl.generarHorario(career, carnet).getCodigoHorario(),165);
 	}
-	
-	@Test
-	@DisplayName("Test utilizando mocks")
-	void testMock() throws Exception{
-		when(registro.cheequearCarnet()).thenReturn(true);
-		when(carrera.procesarCarrera()).thenReturn(1);
-		when(registro.getAnio()).thenReturn(2017);
-		
-		assertEquals(asignar.generarHorario(carrera, registro).getCodigoHorario(),165);
-	}
+
 	
 	@Test
 	@DisplayName("Test con un carnet invalido con mocks")
@@ -82,15 +73,6 @@ class CRISTIANMANUELCASTELLANOSAJTUN201709177 {
 		assertThrows(Exception.class,()-> asignar.generarHorario(carrera, registro));
 	}
 	
-	@Test
-	@DisplayName("Test utilizando spy")
-	void testSpy() throws Exception {
-		when(registro.cheequearCarnet()).thenReturn(true);
-		when(registro.getAnio()).thenReturn(2017);
-		Carrera career = new Carrera(10, "Mecanica");
-		
-		assertEquals(asignarSpy.generarHorario(career, registro).getCodigoHorario(),651);
-	}
 	
 	@Test
 	@DisplayName("Test carnet invalido utilizando spy")
@@ -130,87 +112,5 @@ class CRISTIANMANUELCASTELLANOSAJTUN201709177 {
 		
 	}
 	
-	
-	/*carrera = null;
-	registro = null;
-	horario = null;
-	asignar = null;*/
-
-	/*carrera = new Carrera(9, "Ingenieria en ciencias y sistemas");
-	registro = new RegistroAcademico(2017, "09177");
-	horario = mock(Horario.class);
-	asignar = new AsignarHorario();*/
-	
-	/*try {
-		given(asignar.generarHorario(carrera, registro))
-			.willReturn(horario);
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}*/
-	//fail();
-	// given
-	//Carrera carrera = mock(Carrera.class);
-	//RegistroAcademico carnet = mock(RegistroAcademico.class);
-	
-	/*Carrera carrera = new Carrera(8, "Ingenieria en ciencias y sistemas");
-	RegistroAcademico carnet = new RegistroAcademico(2017, "09177");
-	AsignarHorario assign = mock(AsignarHorario.class);
-	try {
-		when(assign.generarHorario(carrera,carnet))
-			.thenReturn(new Horario(8,"Descripcion"));
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		System.out.println("Error");
-	}*/
-	
-	//verify(assign).generarHorario(carrera, carnet);
-	
-	/*@Test
-	void ShouldFail() {
-		fail("fail test");
-	}*/
-	
-	/*Carrera wrongCareer = new Carrera(13, "Unknow");
-	Carrera correctCareer = new Carrera(10, "Ingenieria en ciencias y sistemas");
-	RegistroAcademico correctRegister = new RegistroAcademico(2017, "09177");
-	RegistroAcademico wrongRegister = new RegistroAcademico(2017, "201709177");
-	RegistroAcademico wrongRegisterWrongYear = new RegistroAcademico(1889, "A9C77");
-	AsignarHorario a = new AsignarHorario();
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	
-
-	@Test
-	@DisplayName("Should throws error invalid register")
-	void ShouldThrowsErrorInvalidRegister() {
-		assertThrows(Exception.class, ()->a.generarHorario(correctCareer, wrongRegister));
-	}
-	
-	@Test
-	@DisplayName("Should throws error invalid carrer")
-	void ShouldThrowsErrorInvalidCareer() {
-		try {
-			assertThrows(Exception.class,()->a.generarHorario(wrongCareer, correctRegister));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	@DisplayName("Should throws error invalid year on career")
-	void ShouldThrowsErrorInvalidYearOnCareer() {
-		try {
-			assertThrows(Exception.class,()->a.generarHorario(wrongCareer, wrongRegisterWrongYear));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}*/
 	
 }
